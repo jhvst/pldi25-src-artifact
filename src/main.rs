7be21f3ed8b457d8a7f55458b7a22d616607fb71
Juuso Haavisto <juuso@ponkila.com>
2025-09-16 12:06:41 +0100

    treewide: fix dune build, cargo example

--- src/main.rs
+++ src/main.rs
@@ -12,8 +12,6 @@ fn main() {
     let mut a = vec![3, 4, 2, 2, 3, 3];
     let mut b = vec![4, 3, 5, 3, 9, 3];
     let l = a.len();
-    let mut right = vec![0; 2];
-    let mut left = vec![0; 2];
-    pad::minus(&mut b, &mut right, &mut left, l, (), (), ());
-    println!("{:?} {:?} {:?}", b, right, left);
+    pad::minus((), (), &mut a, &mut b, l);
+    println!("{:?} {:?}", a, b);
 }
